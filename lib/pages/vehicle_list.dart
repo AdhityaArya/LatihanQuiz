0f5eff048670787c2c342d00f6ed99dc47e74f6f
AdhityaArya <124230068@student.upnyk.ac.id>
2025-10-01 21:03:09 +0700

    update

--- lib/pages/vehicle_list.dart
+++ lib/pages/vehicle_list.dart
@@ -1,33 +1,83 @@
 import 'package:flutter/material.dart';
-import 'vehicleData.dart';
-import 'vehicle_detail.dart';
+import 'package:latihan_quiz/pages/vehicle_detail.dart';
+import 'package:latihan_quiz/pages/vehicleData.dart';
 
 class VehicleList extends StatelessWidget {
   const VehicleList({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
+    return GridView.builder(
+      padding: const EdgeInsets.all(16.0),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 16,
+        mainAxisSpacing: 16,
+        childAspectRatio: 0.8,
+      ),
       itemCount: vehicleList.length,
       itemBuilder: (context, index) {
-        final vehicle = vehicleList[index];
+        final Vehicle vehicle = vehicleList[index];
+
         return Card(
-          child: ListTile(
-            leading: Image.network(
-              vehicle.imageUrls[0],
-              width: 60,
-              fit: BoxFit.cover,
-            ),
-            title: Text(vehicle.name),
-            subtitle: Text(vehicle.type),
+          clipBehavior: Clip.antiAlias,
+          elevation: 8,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: InkWell(
             onTap: () {
               Navigator.push(
                 context,
                 MaterialPageRoute(
-                  builder: (context) => VehicleDetail(vehicle: vehicle),
+                  builder: (context) {
+                    return VehicleDetail(vehicle: vehicle);
+                  },
                 ),
               );
             },
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Expanded(
+                  child: Image.network(
+                    vehicle.imageUrls[0],
+                    fit: BoxFit.cover,
+                    errorBuilder: (context, error, stackTrace) {
+                      return const Center(
+                        child: Icon(
+                          Icons.broken_image,
+                          size: 40,
+                          color: Colors.grey,
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        vehicle.name,
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      const SizedBox(height: 4),
+                      Text(
+                        vehicle.type,
+                        style: TextStyle(fontSize: 14, color: Colors.grey[600]),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         );
       },
